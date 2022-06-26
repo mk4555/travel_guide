abbf0bec9572ad771ce86cfc72627958b9bcf5fe
Min Kim <minkimcodes@gmail.com>
2022-06-26 19:47:07 -0400

    Place full details

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -37,6 +37,24 @@ const PlaceDetails = ({place}) => {
                 {place && place.cuisine && place.cuisine.map(({ name }) => (
                     <Chip key={name} size="small" label={name} className={classes.chip} />
                 ))}
+                {place && place.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon />{place.address}
+                    </Typography>
+                )}
+                {place && place.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                        <PhoneIcon />{place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
             </CardContent>
         </Card>
     )
